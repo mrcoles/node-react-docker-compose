0cb70ce4139459a68b9cda49ea1a7c970a8da582
Elliot Carey <elliotcareydev@gmail.com>
2023-01-22 10:22:30 +0100

    init

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,10 @@ class App extends Component {
     this.state = {};
   }
 
+  displayUsers = () => {
+
+  } 
+
   componentDidMount() {
     this.callApi()
       .then(res => this.setState(res))
@@ -43,7 +47,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React piss</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
